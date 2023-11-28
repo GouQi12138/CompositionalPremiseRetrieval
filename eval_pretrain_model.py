7a8f4ec9e0b1ea3803048055bf57f0114a4d10e6
Steven Zhong <sicheng.zhong@mail.utoronto.ca>
2023-11-28 13:43:28 -0500

    New formulation of Hit@K #3

--- eval_pretrain_model.py
+++ eval_pretrain_model.py
@@ -47,9 +47,11 @@ def gen_labels_and_scores(hypo_to_premises, index, model):
     return gt_labels, scores
 
 
+### EVALUATION FUNCTIONS ###
+
+
 def compute_hit_at_k(hypo_to_premises, index, scores):
     # In top K samples, count how many are correct premises, divide by total number of correct premises
-    # XXX: Unsure how Hit@K is supposed to be calculated
     hit_10_list = []
     hit_20_list = []
     hit_30_list = []
@@ -91,6 +93,51 @@ def compute_hit_at_k(hypo_to_premises, index, scores):
     hit_50 = sum(hit_50_list)/len(hit_50_list)
     return hit_10, hit_20, hit_30, hit_40, hit_50
 
+def compute_hit_at_k_v2(hypo_to_premises, index, scores):
+    # In top K samples, count how many are correct premises, divide by total number of correct premises
+    hit_10_list = []
+    hit_20_list = []
+    hit_30_list = []
+    hit_40_list = []
+    hit_50_list = []
+    prem_count_list = []
+    for i, query in enumerate(hypo_to_premises):
+        gt_premise_indices = index.get_gt_premises(query)
+        query_score = scores[i]
+        top_ind = np.argpartition(query_score, -50)[-50:]
+        sorted_top_ind = top_ind[np.argsort(query_score[top_ind])]
+        num_matches_10 = 0
+        num_matches_20 = 0
+        num_matches_30 = 0
+        num_matches_40 = 0
+        num_matches_50 = 0
+        for j in range(50):
+            if sorted_top_ind[j] in gt_premise_indices:
+                num_matches_50 += 1
+                if j > 9:
+                    num_matches_40 += 1
+                if j > 19:
+                    num_matches_30 += 1
+                if j > 29:
+                    num_matches_20 += 1
+                if j > 39:
+                    num_matches_10 += 1
+        hit_10_list.append(num_matches_10)
+        hit_20_list.append(num_matches_20)
+        hit_30_list.append(num_matches_30)
+        hit_40_list.append(num_matches_40)
+        hit_50_list.append(num_matches_50)
+        prem_count_list.append(len(gt_premise_indices))
+        if args.debug:
+            if i == 1:
+                break
+    hit_10 = sum(hit_10_list)/sum(prem_count_list)
+    hit_20 = sum(hit_20_list)/sum(prem_count_list)
+    hit_30 = sum(hit_30_list)/sum(prem_count_list)
+    hit_40 = sum(hit_40_list)/sum(prem_count_list)
+    hit_50 = sum(hit_50_list)/sum(prem_count_list)
+    return hit_10, hit_20, hit_30, hit_40, hit_50
+
 
 def evaluate_pretrained_model(model, index, hypo_to_premises):
 
@@ -108,7 +155,7 @@ def evaluate_pretrained_model(model, index, hypo_to_premises):
     ndcg_40 = ndcg_score(gt_labels, scores, k=40)
     ndcg_50 = ndcg_score(gt_labels, scores, k=50)
 
-    hit_10, hit_20, hit_30, hit_40, hit_50 = compute_hit_at_k(hypo_to_premises, index, scores)
+    hit_10, hit_20, hit_30, hit_40, hit_50 = compute_hit_at_k_v2(hypo_to_premises, index, scores)
 
     return map_, ndcg, ndcg_10, ndcg_20, ndcg_30, ndcg_40, ndcg_50, hit_10, hit_20, hit_30, hit_40, hit_50
 
