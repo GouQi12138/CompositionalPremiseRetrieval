59f3a6fa740a6dfc5dc3e3c4a0954323daea704f
Steven Zhong <sicheng.zhong@mail.utoronto.ca>
2023-12-12 18:47:12 -0500

    Compositional training framework; working on data

---
+++ train_composition.py
@@ -0,0 +1,107 @@
+import os
+import argparse
+from tqdm import tqdm
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+from torch.utils.data import DataLoader
+from info_nce import InfoNCE, info_nce
+
+from sentence_transformers import SentenceTransformer, InputExample, models
+from sentence_transformers.evaluation import RerankingEvaluator
+from sentence_transformers import losses
+
+from dataloader.pairwise_dataloader import PairwiseDataset, TripletDataset, TripletEvalDataset
+from dataloader.compositional_dataloader import CompositionalDataset, CompositionalEvalDataset
+from dataloader.premise_pool_loader import load_premise_pool
+from eval_pretrain_model import evaluate
+
+from util.loss_functions import CompositionalLoss, ContrastiveRegLoss
+
+
+
+# ---------- Framework ready ----------
+
+def gen_projection_model(model, device):
+    fc1 = models.Dense(in_features=768, out_features=512, activation_function=torch.nn.ReLU())
+    fc2 = models.Dense(in_features=512, out_features=256, activation_function=torch.nn.ReLU())
+    return SentenceTransformer(modules=[model[0], model[1], fc1, fc2]).to(device)
+
+
+def main(args):
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    # Load Model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SentenceTransformer(args.model).to(device)
+
+    # Projection model
+    model = gen_projection_model(model, device)
+
+    if args.debug:
+        print(model)
+
+
+    # Process data format
+    train_dataset = CompositionalDataset(os.path.join(os.getcwd(), args.train_data), contrastive=False)
+    reg_dataset = CompositionalDataset(os.path.join(os.getcwd(), args.train_data), contrastive=True)
+    val_dataset = CompositionalEvalDataset(os.path.join(os.getcwd(), args.val_data))
+
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    reg_dataloader = DataLoader(reg_dataset, batch_size=args.batch_size, shuffle=True)
+    #val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
+
+
+    # info NCE loss
+    #loss = InfoNCE(negative_mode='paired')
+    # Triplet loss
+    #loss = losses.TripletLoss(model=query_model, distance_metric=losses.TripletDistanceMetric.COSINE, triplet_margin=0.001)
+    
+    train_loss = CompositionalLoss(model=model, batch=args.batch_size)
+    reg_loss = ContrastiveRegLoss(model=model, batch=args.batch_size)
+
+
+    # Evaluator
+    # DP retrieval is too expensive; consider:
+    # validation loss
+    # regularization loss
+    # DP retrieval on task2
+    evaluator = RerankingEvaluator(val_dataset)
+    
+
+    # Train
+    model.fit(train_objectives=[(train_dataloader, train_loss), (reg_dataloader, reg_loss)],
+                    evaluator=evaluator,
+                    evaluation_steps=10000,
+                    epochs=args.epochs,
+                    #optimizer_params={'lr': args.learning_rate},
+                    output_path=args.save_dir)
+    
+
+    raise Exception("Finish training.")
+
+
+    # Test
+    query_model = SentenceTransformer(args.save_dir).to(device)
+    evaluateCompositional(query_model, query_model)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="all-mpnet-base-v2",
+                        help="pre-trained model, ./checkpoints/triplet_adjacent or root_leaf \
+                            (best general purpose model: all-mpnet-base-v2 (https://huggingface.co/sentence-transformers/all-mpnet-base-v2); \
+                            best semantic search model: multi-qa-mpnet-base-dot-v1 (https://huggingface.co/sentence-transformers/multi-qa-mpnet-base-dot-v1))")
+    parser.add_argument("--train-data", type=str, default="data/processed_pairwise/pairwise_data_train_triplet_root_leaf_cross_join.tsv") # TODO: replace with compositional data
+    parser.add_argument("--val-data", type=str, default="data/processed_pairwise/pairwise_data_dev_triplet_root_leaf_cross_join.tsv")
+    parser.add_argument("--batch-size", type=int, default=32)
+    parser.add_argument("--epochs", type=int, default=8)
+    parser.add_argument("--learning-rate", type=float, default=1e-6) # 2e-5
+    parser.add_argument("--save-dir", type=str, default="./checkpoints/compositional/", help="directory to save best model weights and loss curves in")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    main(args)
--- util/loss_functions.py
+++ util/loss_functions.py
@@ -1,7 +1,136 @@
+import torch
+from torch import nn, Tensor
+from typing import Union, Tuple, List, Iterable, Dict
+import torch.nn.functional as F
+from sentence_transformers import SentenceTransformer
 
 
 from info_nce import InfoNCE, info_nce
 
+
+
+class CompositionalLoss(nn.Module):
+    """
+    This class implements compositional loss. Given a pair of (hypo, path),
+    the loss minimizes the distance between hypo and path.
+    It compute the following loss function:
+
+    loss = ||hypo - sum(path)||.
+
+    :param model: SentenceTransformerModel
+
+    Example::
+        from sentence_transformers import SentenceTransformer,  SentencesDataset, LoggingHandler, losses
+        from sentence_transformers.readers import InputExample
+
+        model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+        train_examples = [InputExample(texts=['Anchor 1', 'Positive 1', 'Negative 1']),
+            InputExample(texts=['Anchor 2', 'Positive 2', 'Negative 2'])]
+        train_dataset = SentencesDataset(train_examples, model)
+        train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=train_batch_size)
+        train_loss = losses.TripletLoss(model=model)
+    """
+    def __init__(self, model: SentenceTransformer, batch=16):
+        super(CompositionalLoss, self).__init__()
+        self.model = model
+        self.batch = batch
+
+    def get_config_dict(self):
+        return {}
+
+    def forward(self, sentence_features: Iterable[Dict[str, Tensor]], labels: Tensor):
+        # 1 hypo matrix + 32 path matrices
+        assert len(sentence_features) == self.batch + 1
+
+        reps = [self.model(sentence_feature)['sentence_embedding'] for sentence_feature in sentence_features]
+
+        rep_hypo = reps[0]  # batch * 256
+
+        # sum each path and stack paths
+        rep_path = torch.stack([torch.sum(rep_path, dim=0) for rep_path in reps[1:]], dim=0)  # batch * 256
+
+        # compute loss
+        # dist(hypo - path)
+        # https://stackoverflow.com/questions/76979995/pytorch-mse-loss-differs-from-direct-calculation-by-factor-of-2
+        #losses = F.pairwise_distance(rep_hypo, rep_path, p=2).square().mean()   # mean over batch
+        losses = F.mse_loss(rep_hypo, rep_path, reduction='sum') / rep_hypo.shape[0]  # mean over batch  # equivalently
+        
+        return losses
+
+
+
+class ContrastiveRegLoss(nn.Module):
+    """
+    This class implements triplet loss. Given a triplet of (anchor, positive, negative),
+    the loss minimizes the distance between anchor and positive while it maximizes the distance
+    between anchor and negative. It compute the following loss function:
+
+    loss = max(||anchor - positive|| - ||anchor - negative|| + margin, 0).
+
+    Margin is an important hyperparameter and needs to be tuned respectively.
+
+    For further details, see: https://en.wikipedia.org/wiki/Triplet_loss
+
+    :param model: SentenceTransformerModel
+    :param distance_metric: Function to compute distance between two embeddings. The class TripletDistanceMetric contains common distance metrices that can be used.
+    :param triplet_margin: The negative should be at least this much further away from the anchor than the positive.
+
+    Example::
+
+        from sentence_transformers import SentenceTransformer,  SentencesDataset, LoggingHandler, losses
+        from sentence_transformers.readers import InputExample
+
+        model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+        train_examples = [InputExample(texts=['Anchor 1', 'Positive 1', 'Negative 1']),
+            InputExample(texts=['Anchor 2', 'Positive 2', 'Negative 2'])]
+        train_dataset = SentencesDataset(train_examples, model)
+        train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=train_batch_size)
+        train_loss = losses.TripletLoss(model=model)
+    """
+    def __init__(self, model: SentenceTransformer, triplet_margin: float = 1, batch=16):
+        super(ContrastiveRegLoss, self).__init__()
+        self.model = model
+        self.triplet_margin = triplet_margin
+        self.batch = batch
+        #self.loss = CompositionalLoss(model)
+
+    def get_config_dict(self):
+        return {}
+
+    def forward(self, sentence_features: Iterable[Dict[str, Tensor]], labels: Tensor):
+        # organize input and pass to compositional loss
+
+        # 1 hypo matrix + 32 path matrices + 32 negative path matrices
+        assert len(sentence_features) == 2 * self.batch + 1
+
+        reps = [self.model(sentence_feature)['sentence_embedding'] for sentence_feature in sentence_features]
+
+        batch_size = reps[0].shape[0]
+
+        rep_hypo = reps[0]  # batch * 256
+        pos_reps = reps[1:(batch_size+1)] # batch
+        neg_reps = reps[(batch_size+1):]  # batch
+
+        # sum each path and stack paths
+        rep_pos = torch.stack([torch.sum(rep_pos, dim=0) for rep_pos in pos_reps], dim=0)   # batch * 256
+        rep_neg = torch.stack([torch.sum(rep_neg, dim=0) for rep_neg in neg_reps], dim=0)   # batch * 256
+
+        # compute loss
+        # max(||anchor - positive|| - ||anchor - negative|| + margin, 0)
+        
+        #pos_dist = F.pairwise_distance(rep_hypo, rep_pos, p=2).square()
+        #neg_dist = F.pairwise_distance(rep_hypo, rep_neg, p=2).square()
+        pos_dist = F.mse_loss(rep_hypo, rep_pos, reduction='none').sum(dim=1)
+        neg_dist = F.mse_loss(rep_hypo, rep_neg, reduction='none').sum(dim=1)
+
+        losses = F.relu(pos_dist - neg_dist + self.triplet_margin)
+        
+        return losses.mean()
+    
+
+
+
+# ---------- InfoNCE ----------
 
 # https://github.com/RElbers/info-nce-pytorch
 # explicitly paired, supervised
