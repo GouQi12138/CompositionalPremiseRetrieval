3855a682e9ec22494064a6bea4d94bde8022aea4
Anthonyjlem <32406337+Anthonyjlem@users.noreply.github.com>
2023-12-13 21:49:36 -0500

    Alem/brute force (#14)
    * Add brute-force retrieval method
    
    * Remove extra comments

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -65,7 +67,7 @@ class Index:
 
     def __init__(self, hypo_to_premises, premise_pool, model):
 
-        # construct N x dim premise table
+        # construct N x D (embedding_dim) premise table
         # construct hypo to which indices in table are matching
         self._index = []
         self._hypo_to_indices = {}
@@ -110,7 +112,7 @@ class Index:
         return gt_premise_idx_one_hot
 
     def get_similarity_scores(self, query_embedding):
-        # Returns matrix 1 x embedding_dim
+        # Returns matrix 1 x N
         return cosine_similarity(np.expand_dims(query_embedding, axis=0), self._index)
 
     def get_embedding(self, premises):
@@ -120,3 +122,69 @@ class Index:
             embeddings.append(self._premise_to_embedding[p])
         embeddings = np.stack(embeddings)
         return embeddings
+
+    def _gen_all_binary_combinations_of_premises(self):
+        """
+        Returns a binary table representing all possible ocmbinations of premises where each row is a possible combination.
+
+        Ex: For a premise pool of size 3, returns
+            [[0 0 0]
+             [0 0 1]
+             [0 1 0]
+             [0 1 1]
+             [1 0 0]
+             [1 0 1]
+             [1 1 0]
+             [1 1 1]]
+        """
+        combinations = [i for i in product(range(2), repeat=self._index.shape[0])]
+        return np.array(combinations)
+
+    def get_relative_scores(self, query_embedding, solver="brute_force", debug=False):
+        # Returns matrix 1 x N
+        # Generate a relative score for every premise by solving the least squares problem between the query_embedding and the sum of arbitrary premise embeddings
+        if solver != "brute_force":
+            raise NotImplementedError("Only brute force solver is implemented")
+
+        # Generate all possible combinations of premises
+        combinations = self._gen_all_binary_combinations_of_premises()  # 2^N x N
+
+        # Calculate all possible sums
+        sums = combinations @ self._index  # 2^N x D
+
+        # Calcualte all possible differences
+        squared_diffs = ((np.expand_dims(query_embedding, axis=0) - sums)**2).sum(axis=1)  # 2^N
+        if debug:
+            print("Squared diffs:")
+            print(squared_diffs)
+
+        # Rank the differences from least to greatest
+        sorted_idx = np.argsort(squared_diffs)
+
+        # Iterate over the solutions from least difference to greatest difference until all premises have been iterated over
+        # Rank premises from 1 to infinity, incrementing by 1
+        premise_ranks = np.zeros(self._index.shape[0], dtype=int)  # keeps track of which premises have been returned
+        rank = 1
+        for i in sorted_idx:
+            binary_chain = combinations[i]
+
+            # In each solution, organize premises from largest norm to smallest
+            premise_chain = self._index[binary_chain.astype(bool)]  # C (chain length) x D
+            squared_norm = (premise_chain**2).sum(axis=1)
+            sorted_chain_idx = np.argsort(squared_norm)[::-1]
+            for j in sorted_chain_idx:
+                if premise_ranks[j] < 1:
+                    premise_ranks[j] = rank
+                    rank += 1
+
+            # Check if all premises have been assigned ranks
+            if np.all(premise_ranks):
+                break
+        if debug:
+            print("Premise ranks:")
+            print(premise_ranks)
+
+        # Convert ranks to similarity score
+        similarity_scores = 1 - premise_ranks/rank
+        similarity_scores = np.expand_dims(similarity_scores, axis=0)
+        return similarity_scores
