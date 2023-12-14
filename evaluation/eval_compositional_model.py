ddd909ebefd355ff4c4548e337529e9a5d127dae
alem <anthony.lem@mail.utoronto.ca>
2023-12-13 21:47:40 -0500

    Add brute-force retrieval method

---
+++ evaluation/eval_compositional_model.py
@@ -0,0 +1,294 @@
+import argparse
+import time
+
+import numpy as np
+from prettytable import PrettyTable
+from sklearn.metrics import average_precision_score, ndcg_score
+from sentence_transformers import SentenceTransformer
+import torch
+from tqdm import tqdm
+
+from dataloader.premise_evaluation_loader import load_target_dict
+from dataloader.premise_pool_loader import load_premise_pool
+from index import *
+import time
+
+
+### DEBUG FUNCTIONS ###
+
+
+def check_map(gt_labels, scores):
+    print("GT labels:")
+    print(gt_labels)
+    print("Scores:")
+    print(scores)
+    print("Scores of gt_labels:")
+    print(scores[0][np.nonzero(gt_labels[0].astype(int))])
+    print(scores[1][np.nonzero(gt_labels[1].astype(int))])
+    print("Ranked scores")
+    print(np.sort(scores)[:,-40:])
+
+
+### HELPER FUNCTIONS ###
+
+
+def gen_labels_and_scores(hypo_to_premises, index, query_model, debug=False):
+    # Encode each query
+    # Generate relative ranking for all the premises for each query
+    gt_labels = []
+    scores = []
+    for i, query in enumerate(tqdm(hypo_to_premises)):
+        gt_premise_indices = index.get_gt_premises(query, one_hot=True)
+        gt_labels.append(gt_premise_indices)
+        with torch.no_grad():
+            query_embedding = query_model.encode(query)
+        if debug:
+            print("Query Embedding:")
+            print(query_embedding)
+        # similarity_scores = index.get_similarity_scores(query_embedding)
+        similarity_scores = index.get_relative_scores(query_embedding, debug=debug)
+        scores.append(similarity_scores)
+    gt_labels = np.stack(gt_labels)
+    scores = np.concatenate(scores)
+    return gt_labels, scores
+
+
+### EVALUATION FUNCTIONS ###
+
+
+def k_at_recall(premises, predictions, recall_level=1):
+    # Recall is the number of relevant documents retrieved divided by the total number of relevant documents
+    # Relevant documents are those that are in the ground truth
+    # Retrieved documents are those that are in the top K samples
+    items_left = len(premises)
+    k = 0
+    for idx in predictions:
+        k += 1
+        if idx in premises:
+            items_left -= 1
+        if items_left == 0:
+            return k
+    raise Exception("Not all premises are retrieved")
+
+def compute_precision_at_full_recall(hypo_to_premises, faissIndex, query_model=None):
+    # Precision at full recall is the average of the precision values at each recall threshold
+    # Recall threshold is the number of relevant documents
+    # Precision is the number of relevant documents divided by the number of retrieved documents
+    # Relevant documents are those that are in the ground truth
+    # Retrieved documents are those that are in the top K samples
+    precision_at_full_recall_list = []
+    for h in hypo_to_premises:
+        prem_label = faissIndex._hypo_to_indices[h]
+        prem_pred = faissIndex.retrieve_index(h, k=len(faissIndex._premise_pool), model=query_model)
+
+        num_relevant_docs = len(hypo_to_premises[h])
+        num_retrieved_docs = k_at_recall(prem_label, prem_pred)
+        precision_at_full_recall_list.append(num_relevant_docs/num_retrieved_docs)
+    precision_at_full_recall = sum(precision_at_full_recall_list)/len(precision_at_full_recall_list)
+    return precision_at_full_recall
+
+
+def compute_hit_at_k(hypo_to_premises, index, scores):
+    # In top K samples, count how many are correct premises, divide by total number of correct premises
+    hit_10_list = []
+    hit_20_list = []
+    hit_30_list = []
+    hit_40_list = []
+    hit_50_list = []
+    for i, query in enumerate(hypo_to_premises):
+        gt_premise_indices = index.get_gt_premises(query)
+        query_score = scores[i]
+        top_ind = np.argpartition(query_score, -50)[-50:]
+        sorted_top_ind = top_ind[np.argsort(query_score[top_ind])]
+        num_matches_10 = 0
+        num_matches_20 = 0
+        num_matches_30 = 0
+        num_matches_40 = 0
+        num_matches_50 = 0
+        for j in range(50):
+            if sorted_top_ind[j] in gt_premise_indices:
+                num_matches_50 += 1
+                if j > 9:
+                    num_matches_40 += 1
+                if j > 19:
+                    num_matches_30 += 1
+                if j > 29:
+                    num_matches_20 += 1
+                if j > 39:
+                    num_matches_10 += 1
+        hit_10_list.append(num_matches_10/len(gt_premise_indices))
+        hit_20_list.append(num_matches_20/len(gt_premise_indices))
+        hit_30_list.append(num_matches_30/len(gt_premise_indices))
+        hit_40_list.append(num_matches_40/len(gt_premise_indices))
+        hit_50_list.append(num_matches_50/len(gt_premise_indices))
+        if args.debug:
+            if i == 1:
+                break
+    hit_10 = sum(hit_10_list)/len(hit_10_list)
+    hit_20 = sum(hit_20_list)/len(hit_20_list)
+    hit_30 = sum(hit_30_list)/len(hit_30_list)
+    hit_40 = sum(hit_40_list)/len(hit_40_list)
+    hit_50 = sum(hit_50_list)/len(hit_50_list)
+    return hit_10, hit_20, hit_30, hit_40, hit_50
+
+def compute_hit_at_k_v2(hypo_to_premises, index, scores, debug=False):
+    # In top K samples, count how many are correct premises, divide by total number of correct premises
+    hit_10_list = []
+    hit_20_list = []
+    hit_30_list = []
+    hit_40_list = []
+    hit_50_list = []
+    prem_count_list = []
+    for i, query in enumerate(hypo_to_premises):
+        gt_premise_indices = index.get_gt_premises(query)
+        query_score = scores[i]
+        top_ind = np.argpartition(query_score, -50)[-50:]
+        sorted_top_ind = top_ind[np.argsort(query_score[top_ind])]
+        num_matches_10 = 0
+        num_matches_20 = 0
+        num_matches_30 = 0
+        num_matches_40 = 0
+        num_matches_50 = 0
+        for j in range(50):
+            if sorted_top_ind[j] in gt_premise_indices:
+                num_matches_50 += 1
+                if j > 9:
+                    num_matches_40 += 1
+                if j > 19:
+                    num_matches_30 += 1
+                if j > 29:
+                    num_matches_20 += 1
+                if j > 39:
+                    num_matches_10 += 1
+        hit_10_list.append(num_matches_10)
+        hit_20_list.append(num_matches_20)
+        hit_30_list.append(num_matches_30)
+        hit_40_list.append(num_matches_40)
+        hit_50_list.append(num_matches_50)
+        prem_count_list.append(len(gt_premise_indices))
+        if debug:
+            if i == 1:
+                break
+    hit_10 = sum(hit_10_list)/sum(prem_count_list)
+    hit_20 = sum(hit_20_list)/sum(prem_count_list)
+    hit_30 = sum(hit_30_list)/sum(prem_count_list)
+    hit_40 = sum(hit_40_list)/sum(prem_count_list)
+    hit_50 = sum(hit_50_list)/sum(prem_count_list)
+    return hit_10, hit_20, hit_30, hit_40, hit_50
+
+
+def evaluate_model(query_model, index, hypo_to_premises, faissIndex, debug=False):
+
+    # Solve Knapsack problem:
+    # https://developers.google.com/optimization/pack/knapsack
+    # https://developers.google.com/optimization/reference/python/algorithms/pywrapknapsack_solver
+    # https://pyeasyga.readthedocs.io/en/latest/examples.html
+    # Get top-k chains, where k is the number of premises in the corpus for fair comparison against non chain methods
+        # No it won't be a fair comparison. Maybe return like the number of premises rounded up
+        # Unfortunately won't be a fair comparison because the number of premises being evaluated is inconsistent
+            # To make it fair, would need to keep the number of premises the same between the two
+            # Unless I can ensure that the algorithm returns solutions until it encapsulates all of the premises
+    # But how do we deal with repeated premises?
+        # Either we ignore repeated premises, or we just evaluate each chain
+
+    # Options for similarity: - don't need this
+    # s = 1 - d/d_max; this retains the distribution; from https://journals.ametsoc.org/view/journals/mwre/129/3/1520-0493_2001_129_0540_edaasm_2.0.co_2.xml
+    # change to s = max(1 - d/d_max, 0)
+    # s = np.exp(-D * gamma), where one heuristic for choosing gamma is 1 / num_features; from https://scikit-learn.org/stable/modules/metrics.html#metrics; this will change the distribution
+
+    # How to get similarity for each premise within a set?
+
+    print("Evaluating...")
+    query_model.eval()
+    gt_labels, scores = gen_labels_and_scores(hypo_to_premises, index, query_model, debug=debug)  # scores is not confidence/similarity scores, but ranking scores
+    if debug:
+        print("Scores")
+        print(scores)
+
+    prec_full_rec = compute_precision_at_full_recall(hypo_to_premises, faissIndex, query_model)  # XXX: pass relative rankings into here
+
+    map_ = average_precision_score(gt_labels, scores, average="samples")  # Okay for scores to be relative rankings, will not estimate AP
+    if debug:
+        check_map(gt_labels, scores)
+
+    # Assume okay for scores to be relative rankings since ndcg_score is done at different k
+    ndcg = ndcg_score(gt_labels, scores)
+    ndcg_10 = ndcg_score(gt_labels, scores, k=10)
+    ndcg_20 = ndcg_score(gt_labels, scores, k=20)
+    ndcg_30 = ndcg_score(gt_labels, scores, k=30)
+    ndcg_40 = ndcg_score(gt_labels, scores, k=40)
+    ndcg_50 = ndcg_score(gt_labels, scores, k=50)
+
+    # Okay for score to be relative rankings for compute hit at k
+    hit_10, hit_20, hit_30, hit_40, hit_50 = compute_hit_at_k_v2(hypo_to_premises, index, scores, debug=debug)
+
+    return prec_full_rec, map_, ndcg, ndcg_10, ndcg_20, ndcg_30, ndcg_40, ndcg_50, hit_10, hit_20, hit_30, hit_40, hit_50
+
+
+def evaluate(premise_model, query_model, split="test", debug=False):
+    hypo_to_premises = load_target_dict(split)
+    premise_pool = load_premise_pool()
+    if args.debug:
+        small_hypo_to_premises = {}
+        for k in list(hypo_to_premises.keys())[:3]:
+            small_hypo_to_premises[k] = hypo_to_premises[k]
+        hypo_to_premises = small_hypo_to_premises
+        premise_pool = []
+
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print("Start Indexing...", current_time)
+    index = Index(hypo_to_premises, premise_pool, premise_model)
+    faissIndex = FaissIndex(hypo_to_premises, premise_pool, premise_model)
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print("Indexing Done", current_time)
+    
+    res = evaluate_model(query_model, index, hypo_to_premises, faissIndex, debug=debug)
+    tab = PrettyTable()
+    tab.field_names = ["Prec@Full Recall", "MAP", "NDCG", "NDCG@10", "NDCG@20", "NDCG@30", "NDCG@40", "NDCG@50", "Hit@10", "Hit@20", "Hit@30", "Hit@40", "Hit@50"]
+    tab.add_row(["{0:0.3f}".format(i) for i in res])
+    print(tab)
+
+
+def retrieve(premise_model):
+    hypo_to_premises = load_target_dict("test")
+    premise_pool = load_premise_pool()
+    
+    faissIndex = FaissIndex(hypo_to_premises, premise_pool, premise_model)
+    
+    for h in hypo_to_premises:
+        true_premises = hypo_to_premises[h]
+        pred_premises = faissIndex.retrieve_index(h, k=20)
+
+        print("Hypothesis:", h)
+        print("True premises:")
+        for i in true_premises:
+            print(i)
+        print("Predicted premises:")
+        for i in pred_premises:
+            print(premise_pool[i])
+
+        break
+
+def main(args):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SentenceTransformer(args.model).to(device)
+    if not args.debug:
+        print("Loading model from checkpoint...")
+        model.load_state_dict(torch.load(args.model_path))
+    evaluate(model, model, split=args.split, debug=args.debug)
+    if args.debug:
+        retrieve(model)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="all-mpnet-base-v2",
+                        help="model architecture \
+                            (best general purpose model: all-mpnet-base-v2 (https://huggingface.co/sentence-transformers/all-mpnet-base-v2); \
+                            best semantic search model: multi-qa-mpnet-base-dot-v1 (https://huggingface.co/sentence-transformers/multi-qa-mpnet-base-dot-v1))")
+    parser.add_argument("--model-path", type=str, help="model weights (used for both premise and query models)")
+    parser.add_argument("--split", type=str, default="test", help="data split to evaluate on (train, dev, test)")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    main(args)
